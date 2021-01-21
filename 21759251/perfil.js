46a63a9348c3aea83dd54a0cd91a02937770a14b
Johan Velasquez Piedrahita <johan@MacBook-Air-de-Johan.local>
2021-01-20 20:38:47 -0400

    entrega reto 10

---
+++ 21759251/perfil.js
@@ -0,0 +1,14 @@
+let perfil = {
+	"nombre":"Johan Velasquez",
+	"descripcion":"Estatura media, piel oscura y pelo negro.",
+	"color":"negro",
+	"libro":"El Señor de los Anillos",
+	"musica":"Rock",
+	"video_juego": ["Hearts of Iron"],
+	"lenguajes":["C", "Java", "Swift"],
+	"email":"johanvelasquezpiedrahita@gmail.com",
+	"ci":"21759251",
+	"genero": "masculino",
+	"fecha_nacimiento":"16/10/1994",
+	"imagen": "21759251.jpg"
+}
