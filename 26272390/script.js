b53db037f0ee16aa9a0bbf5893d69be5fb888b8a
Daniel <danielviei@hotmail.com>
2021-05-12 20:44:23 -0400

    archivos reto 14

--- 26272390/script.js
+++ 26272390/script.js
@@ -1,12 +1,15 @@
-document.getElementById("saludo").innerHTML = config.saludo + perfil.nombre;
-document.getElementById("sitio").innerHTML = config.sitio[0] + "<small>" + config.sitio[1] + "</small>" + config.sitio[2]
-document.getElementById("inicio").innerHTML = config.home;
-document.getElementById("nombre").innerHTML = perfil.nombre;
-document.getElementById("descripcion").innerHTML = perfil.descripcion;
-document.getElementById("column1").innerHTML = config.color + "<br/>" + config.libro + "<br/>" + config.musica + "<br/>" + config.video_juego + "<br/>";
-document.getElementById("column2").innerHTML = perfil.color + "<br/>" + perfil.libro + "<br/>" + perfil.musica + "<br/>" + perfil.video_juego + "<br/>";
-document.getElementById("lenguajes").innerHTML = config.lenguajes;
-document.getElementById("mis_lenguajes").innerHTML = perfil.lenguajes;
-document.getElementById("contacto").innerHTML = config.email.replace("[email]",  "<a href=\"mailto:danielvieiucv@gmail.com\"> "+ perfil.email + "</a>");
-document.getElementById("footer").innerHTML = config.copyRight;
-document.getElementById("imagen").setAttribute("src",  perfil.imagen);
+$("#saludo").html(config.saludo + perfil.nombre);
+$("#sitio").html(config.sitio[0] + "<small>" + config.sitio[1] + "</small>" + config.sitio[2]);
+$("#inicio").html(config.home);
+$("#nombre").html(perfil.nombre);
+$("#descripcion").html(perfil.descripcion);
+$("#column1").html(config.color + "<br/>" + config.libro + "<br/>" + config.musica + "<br/>" + config.video_juego + "<br/>");
+$("#column2").html(perfil.color + "<br/>" + perfil.libro + "<br/>" + perfil.musica + "<br/>" + perfil.video_juego + "<br/>");
+$("#lenguajes").html(config.lenguajes);
+$("#mis_lenguajes").html(perfil.lenguajes[0]);
+for(i = 1; i < 4; ++i) {
+    $("#mis_lenguajes").html($("#mis_lenguajes").html() + ", " + perfil.lenguajes[i]);
+}
+$("#contacto").html(config.email.replace("[email]", "<a href=\"mailto:danielvieiucv@gmail.com\"> "+ perfil.email + "</a>"));
+$("#footer").html(config.copyRight);
+$("#imagen").attr("src", perfil.imagen);
