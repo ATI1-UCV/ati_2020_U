fbebc329a9b46b9767f1ad1bf1007d9e76fabd3e
Gabriela Ustariz <gabrielaustariz@gmail.com>
2021-05-21 00:56:16 -0400

    carousel done

--- js/index.js
+++ js/index.js
@@ -7,28 +7,80 @@ window.onload = function() {
     document.getElementsByClassName('logo')[0].innerHTML = config.sitio[0] + '<small>' + config.sitio[1] +
         '</small>' + config.sitio[2];
     document.getElementsByClassName('saludo')[0].innerHTML = config.saludo + ', ' + estudiantes[0].nombre;
-    let lista = '<ul id="myUL">'
+
+    const ul = document.createElement('DIV');
+    ul.classList.add('myUL');
+    ul.classList.add('carousel-inner');
+    let cont = 0
+
     for (x of estudiantes) {
+        
+        const container_image = document.createElement('DIV');
+        container_image.classList.add('carousel-item');
+        if (cont++ == 0){container_image.classList.add('active');}
+        // div.setAttribute('data-interval', '10000');
+
+        const image = document.createElement('IMG');
+        image.src = x.imagen;
+        image.alt = `foto de ${x.nombre}`;
+
+        const parrafo = document.createElement('P');
+        const anchor= document.createElement('A');
+        parrafo.textContent = x.nombre;
         if (x.ci == '26956071') {
-            lista += '<li><img src=\"' + x.imagen + '\" ' + 'alt=\"foto de ' + x.nombre + '\" />' +
-                '<a href=\"26956071/perfil.html\">' + x.nombre + '</a></li>'
-        } else {
-            lista += '<li><img src=\"' + x.imagen + '\" ' + 'alt=\"foto de ' + x.nombre + '\" />' +
-                '<a href=\"#\">' + x.nombre + '</a></li>';
-        }
+            anchor.href = `${x.ci}/perfil.html`;
+        } 
+        else {anchor.href = '#';}
+        const container2 = document.createElement('DIV')
+        anchor.appendChild(image);
+        anchor.appendChild(parrafo);
+        anchor.classList.add('container-visible');
+        console.log(container2)
+        container2.appendChild(anchor)
+        container_image.appendChild(container2)
+        ul.appendChild(container_image);
     }
-    lista += '</ul>';
-    console.log(lista);
-    document.getElementsByTagName('section')[0].innerHTML = lista;
+
+    const main = document.getElementsByTagName('section')[0];
+    main.setAttribute('id', 'carouselExampleControls');
+    main.setAttribute('data-ride', 'carousel');
+    main.classList.add('carousel');
+    main.classList.add('slide');
+    main.appendChild(ul);
+
     document.getElementsByTagName('footer')[0].innerHTML = config.copyRight;
+
+    $('#carouselExampleControls').carousel({
+        interval: false
+    });
+    
+    $('.carousel-item').each(function () {
+        var next = $(this).next();
+        // if (!next.length) {
+        //     next = $(this).siblings(':first');
+        // }
+        next.children(':first-child').clone().appendTo($(this));
+    
+        for (var i = 0; i < 2; i++) {
+            next = next.next();
+            // if (!next.length) {
+            //     next = $(this).siblings(':first');
+            // }
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+
+
+
+// 
 }
 
 function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
+    ul = document.getElementsByClassName("myUL")[0];
+    li = ul.getElementsByTagName("DIV");
     var sum = 0;
     var ind = 0;
     for (i = 0; i < li.length; i++) {
