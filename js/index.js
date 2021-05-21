e1235fbf24af07e8c06525e3ef7aa0e1c76d10d4
Katherin Mozo <kathemozo27@gmail.com>
2021-05-21 18:49:21 -0430

    reto 15 - lab

--- js/index.js
+++ js/index.js
@@ -1,65 +1,117 @@
 window.onload = function() {
     //config
     // variables
-    const logo = document.querySelector('.logo')
-    const saludo = document.querySelector('.saludo')
+    const logo = $('.logo')
+    const saludo = $('.saludo')
     const busqueda = document.querySelector('.busqueda').firstChild
-    const footer = document.querySelector('footer')
-    const sectionListado = document.querySelector('section')
-    const input = document.querySelector('#nombreBusqueda');
-    const form = document.querySelector('form')
-    const titulo = document.querySelector('title')
+    const footer = $('footer')
+    const sectionListado = $('.swiper-wrapper')
+    const input = $('#nombreBusqueda');
+    const form = $('form')
+    const titulo = $('title')
     
-   
+    const swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 5,
+        autoplay: {
+            delay: 1000,
+            pauseOnMouseEnter:true,
+          },
+        breakpoints:{
+            // si width es >= 220px
+            220:{
+                slidesPerView: 2,
+                spaceBetween: 5,
+            },
+            // si width es >= 380px
+            380:{
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            // si width es >= 500px
+            500:{
+                slidesPerView: 4,
+                spaceBetween: 5,
+            },
+            // si width es >= 767px
+            767: {
+                slidesPerView: 5,
+                spaceBetween: 10
+            },
+            // si width es >= 1280px
+            1280: {
+                slidesPerView: 6,
+                spaceBetween: 10
+            },
+        },
+      });
+ 
     //cambios ATI[UCV] 2020-U
-    titulo.innerText = `${config.sitio[0]}${config.sitio[1]} ${config.sitio[2]}`
+    titulo.text (`${config.sitio[0]}${config.sitio[1]} ${config.sitio[2]}`)
     //internacionalizacion
-    logo.innerHTML = `${config.sitio[0]}<small>${config.sitio[1]}</small> ${config.sitio[2]}`
-    saludo.innerText = `${config.saludo}, Katherin Mozo`
+    logo.html(`${config.sitio[0]}<small>${config.sitio[1]}</small> ${config.sitio[2]}`)
+    saludo.text(`${config.saludo}, Katherin Mozo`)
     busqueda.nextElementSibling[0].setAttribute("placeholder", config.nombre)
     busqueda.nextElementSibling[1].setAttribute("value", config.buscar)
-    footer.innerText = config.copyRight
+    footer.text(config.copyRight) 
+
+    
+    //listado de estudiantes
+    // for(let i=0; i < listado.length; i++){
+    //     const nuevoDiv = document.createElement('div')
+    //     nuevoDiv.classList.add('swiper-slide')
+    //         nuevoDiv.innerHTML = `
+                
+    //                 <img src="${listado[i].imagen}" alt="${listado[i].nombre}">
+    //                 <a href="./${listado[i].ci}/perfil.html">${listado[i].nombre}</a>
+    //             `
+    //         sectionListado.appendChild(nuevoDiv)
+    // }
+    // swiper.update()
 
     //listado de estudiantes
     for(let i=0; i < listado.length; i++){
-        const nuevoUL = document.createElement('ul')
-            nuevoUL.innerHTML = `
-                <li>
-                    <img src="${listado[i].imagen}" alt="${listado[i].nombre}">
-                    <a href="./${listado[i].ci}/perfil.html">${listado[i].nombre}</a>
-                </li>`
-            sectionListado.appendChild(nuevoUL)
+            sectionListado.append(`
+                 <div class="swiper-slide">
+                     <img src="${listado[i].imagen}" alt="${listado[i].nombre}">
+                     <a href="./${listado[i].ci}/perfil.html">${listado[i].nombre}</a>
+                 </div> `)
     }
-    
-    
-    
-    //busqueda 
-    form.addEventListener('submit',(e)=>{
-        e.preventDefault()
+    swiper.update()
+
+
+   
+
+    form.submit(function(event){
+        event.preventDefault();
     })
 
-    input.addEventListener('keyup', busquedaNombre)
-    function busquedaNombre(){
-        const entrada = this.value
+    
 
-        sectionListado.innerHTML = '';
+    input.keyup(function (){
+        const entrada = $(this).val()
+       
+
+        sectionListado.html('')
         for(let i=0; i < listado.length; i++){
             if(listado[i].nombre.toUpperCase().includes(entrada.toUpperCase())){
-                const nuevoUL = document.createElement('ul')
-                nuevoUL.innerHTML = `
-                    <li>
-                        <img src="${listado[i].imagen}" alt="${listado[i].nombre}">
-                        <a href="./${listado[i].ci}/perfil.html">${listado[i].nombre}</a>
-                    </li>`
-                sectionListado.appendChild(nuevoUL)
+                sectionListado.append(`
+                 <div class="swiper-slide">
+                     <img src="${listado[i].imagen}" alt="${listado[i].nombre}">
+                     <a href="./${listado[i].ci}/perfil.html">${listado[i].nombre}</a>
+                 </div> `)
+
             }
         }
-        if(sectionListado.innerHTML === ''){
+        if(sectionListado.html() === ''){
             let textMensaje = `<p>${config.mensajeBusqueda}</p>`
-            sectionListado.innerHTML = textMensaje.replace('[query]', entrada)
+            sectionListado.html(textMensaje.replace('[query]', entrada))
         }
-        
-    };
+        swiper.update()
+    })
+    
+
+    
     
 
 }
