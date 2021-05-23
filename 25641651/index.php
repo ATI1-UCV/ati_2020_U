cca0e06e65164f9f24b3c73fc55fc4e0531506e4
EduhuSua <Eduardosuarez.ucv@gmail.com>
2021-05-22 20:49:19 -0400

    reto 19

--- 25641651/index.php
+++ 25641651/index.php
@@ -11,15 +11,40 @@
 		<script src="./config/perfil.json"></script>
 		<title>Eduardo Suarez</title>
 	</head>
+
+    <?php
+        $idioma = htmlspecialchars($_GET["len"]);
+        if($idioma == "en"){
+            $data2 = @file_get_contents("config/configEN.json");
+        }
+        else if($idioma == "es"){
+            $data2 = @file_get_contents("config/configES.json");
+        }
+        else if($idioma == "pt"){
+            $data2 = @file_get_contents("config/configPT.json");
+        }else{
+            $data2 = @file_get_contents("config/configES.json");
+        }
+
+        $data1 = @file_get_contents("config/perfil.json");
+        $itemP = json_decode($data1, true);
+        $itemC = json_decode($data2, true);
+    ?>
+
 	<body>
-        
 	    <nav class="navbar navbar-expand-lg navbar-dark bg-primary"> 
             <div class="navbar-nav container-fluid cont-logo">
                 <a class="navbar-brand logo" href="#">ATI[UCV]2020-U</a>
                 <div class="container">
                     <div class="row">
-                        <a class="col navbar-brand text-center saludo" aria-current="page" href="#"></a>
-                        <a class="col navbar-brand text-center busqueda" href="../index.html"></a>
+                        <a class="col navbar-brand text-center saludo" aria-current="page" href="#"><?php
+                            echo $itemC["nombre"];
+                        ?></a>
+                        <a class="col navbar-brand text-center busqueda" href="../index.html">
+                        <?php
+                            echo $itemC["home"];
+                        ?>
+                        </a>
                     </div>    
                 </div>
             </div>
@@ -32,50 +57,62 @@
                     <img src="25641651.jpg" class="img-fluid imagen" alt="">
                 </div>
                 <div class="col-7 shadow p-3 mb-5 ps-5 bg-body rounded">
-                    <h2 id="texto1"></h2>
-                    <p id="texto2"></p>
+                    <h2 id="texto1"><?php echo $itemP["nombre"];?></h2>
+                    <p id="texto2"><?php echo $itemP["descripcion"];?></p>
                     <div class="row">
                         <div class="col">
-                            <p class="colorC"></p>
+                            <p class="colorC"><?php echo $itemC["color"];?></p>
                         </div>
                         <div class="col">
-                            <p class="colorP"></p>
+                            <p class="colorP"><?php echo $itemP["color"];?></p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col">
-                            <p class="libroC"></p>
+                            <p class="libroC"><?php echo $itemC["libro"];?></p>
                         </div>
                         <div class="col">
-                            <p class="libroP"></p>
+                            <p class="libroP"><?php echo $itemP["libro"];?></p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col">
-                            <p class="musicaC"></p>
+                            <p class="musicaC"><?php echo $itemC["musica"];?></p>
                         </div>
                         <div class="col">
-                            <p class="musicaP"></p>
+                            <p class="musicaP"><?php echo $itemP["musica"];?></p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col">
-                            <p class="videoJuegoC"></p>
+                            <p class="videoJuegoC"><?php echo $itemC["video_juego"];?></p>
                         </div>
                         <div class="col">
-                            <p class="videoJuegoP"></p>
+                            <p class="videoJuegoP"><?php echo $itemP["video_juego"];?></p>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col">
-                            <p class="lenguajeC fw-bold"></p>
+                            <p class="lenguajeC fw-bold"><?php echo $itemC["lenguajes"];?></p>
                         </div>
                         <div class="col">
-                            <p class="lenguajeP fw-bold"></p>
+                            <p class="lenguajeP fw-bold">
+                                <?php 
+                                    echo $itemP["lenguajes"][0];
+                                    echo $itemP["lenguajes"][1];
+                                ?>
+                            </p>
                         </div>
                     </div>
-                    <div class="row m-3 w-100 ">
-                        <p class="comu"></p>
+                    <div class="row w-100">
+                        <?php
+                            echo '<p>';
+                            echo $itemC["email"];
+                            echo '</p>';
+                            echo '<a href="https://mail.google.com/mail/?view=cm&fs=1&to=eduardosuarez.ucv@gmail.com" id="texto4">';
+                                echo $itemP["email"];
+                            echo '</a>';
+                        ?>
                     </div>
                 </div>
             </div>
@@ -84,8 +121,8 @@
         <footer class="text-center p-3  bg-primary text-white" style="background-color: rgba(0, 0, 0, 0.2);">
         </footer>
 
-		<script src="./js/jquery-3.6.0.min.js"></script> 
+		<!-- <script src="jquery-3.6.0.min.js"></script>  -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-		<script src="./js/scrip.js"></script>
+		<!-- <script src="scrip.js"></script> -->
 	</body>
 </html>
