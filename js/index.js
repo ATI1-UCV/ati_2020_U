1085d1460c1fdda1046c6ec853b50d74c185e3a8
Gabriela Ustariz <gabrielaustariz@gmail.com>
2021-05-25 02:25:36 -0400

    archivos reto 19

--- js/index.js
+++ js/index.js
@@ -1,48 +1,81 @@
-window.onload = function() {
+window.onload = app;
 
-    config = JSON.parse(JSON.stringify(config));
-    estudiantes = JSON.parse(JSON.stringify(listado));
+var config = JSON.parse(JSON.stringify(config));
+var estudiantes = JSON.parse(JSON.stringify(listado));
+
+
+function app(){
+    header();
+    fillMain();
+    footer();
+}
+
+function header(){
 
     document.title = config.sitio[0] + config.sitio[1] + ' ' + config.sitio[2];
     document.getElementsByClassName('logo')[0].innerHTML = config.sitio[0] + '<small>' + config.sitio[1] +
         '</small>' + config.sitio[2];
     document.getElementsByClassName('saludo')[0].innerHTML = config.saludo + ', ' + estudiantes[0].nombre;
-    let lista = '<ul id="myUL">'
-    for (x of estudiantes) {
-        if (x.ci == '26956071') {
-            lista += '<li><img src=\"' + x.imagen + '\" ' + 'alt=\"foto de ' + x.nombre + '\" />' +
-                '<a href=\"26956071/perfil.html\">' + x.nombre + '</a></li>'
-        } else {
-            lista += '<li><img src=\"' + x.imagen + '\" ' + 'alt=\"foto de ' + x.nombre + '\" />' +
-                '<a href=\"#\">' + x.nombre + '</a></li>';
+
+}
+
+function fillMain(){
+
+
+    const main = document.getElementById('main-container');
+    $('.myUL').remove();
+    var input, filter, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    const lista_estudiantes = document.createElement('DIV')
+    lista_estudiantes.classList.add('myUL');
+    // lista_estudiantes.classList.add('carousel-inner');
+    let cont = 0
+
+    for(x of estudiantes){
+        txtValue = x.nombre;
+        if (txtValue.toUpperCase().indexOf(filter) > -1 || input == '') {
+            const container_image = document.createElement('DIV');
+            // container_image.classList.add('carousel-item');
+            // if (cont++ == 0){container_image.classList.add('active');}
+    
+            const image = document.createElement('IMG');
+            image.src = x.imagen;
+            image.alt = `foto de ${x.nombre}`;
+    
+            const parrafo = document.createElement('P');
+            const anchor= document.createElement('A');
+            parrafo.textContent = x.nombre;
+            if (x.ci == '26956071') {
+                anchor.href = `${x.ci}/perfil.php`;
+            } 
+            else {anchor.href = '#';}
+            const container2 = document.createElement('DIV')
+            anchor.appendChild(image);
+            anchor.appendChild(parrafo);
+            anchor.classList.add('container-visible');
+            container2.appendChild(anchor)
+            container_image.appendChild(container2)
+            lista_estudiantes.appendChild(container_image);
         }
     }
-    lista += '</ul>';
-    console.log(lista);
-    document.getElementsByTagName('section')[0].innerHTML = lista;
+    main.appendChild(lista_estudiantes);
+    console.log(main);
+}
+
+function footer(){
     document.getElementsByTagName('footer')[0].innerHTML = config.copyRight;
 }
 
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    var sum = 0;
-    var ind = 0;
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            sum++;
-            li[i].style.display = "";
-            ind = i;
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-    li[ind].style.marginBlockEnd = '85px';
-    if (!sum) { alert(config.mensaje_error.replace("[query]", input.value)); }
 
+function group(){
+    $('.carousel-item').each(function () {
+        var next = $(this).next();
+        next.children(':first-child').clone().appendTo($(this));
+    
+        for (var i = 0; i < 2; i++) {
+            next = next.next();
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
 }
