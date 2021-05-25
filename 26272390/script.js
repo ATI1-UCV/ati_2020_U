533ec2fa77ab7cf784700b8c13ce849b2d4bb69a
Daniel <danielviei@hotmail.com>
2021-05-24 23:07:24 -0400

    archivos reto 19

--- 26272390/script.js
+++ 26272390/script.js
@@ -1,15 +1,15 @@
-$("#saludo").html(config.saludo + perfil.nombre);
-$("#sitio").html(config.sitio[0] + "<small>" + config.sitio[1] + "</small>" + config.sitio[2]);
-$("#inicio").html(config.home);
-$("#nombre").html(perfil.nombre);
-$("#descripcion").html(perfil.descripcion);
-$("#column1").html(config.color + "<br/>" + config.libro + "<br/>" + config.musica + "<br/>" + config.video_juego + "<br/>");
-$("#column2").html(perfil.color + "<br/>" + perfil.libro + "<br/>" + perfil.musica + "<br/>" + perfil.video_juego + "<br/>");
-$("#lenguajes").html(config.lenguajes);
-$("#mis_lenguajes").html(perfil.lenguajes[0]);
-for(i = 1; i < 4; ++i) {
-    $("#mis_lenguajes").html($("#mis_lenguajes").html() + ", " + perfil.lenguajes[i]);
-}
-$("#contacto").html(config.email.replace("[email]", "<a href=\"mailto:danielvieiucv@gmail.com\"> "+ perfil.email + "</a>"));
-$("#footer").html(config.copyRight);
-$("#imagen").attr("src", perfil.imagen);
+// $("#saludo").html(config.saludo + perfil.nombre);
+// $("#sitio").html(config.sitio[0] + "<small>" + config.sitio[1] + "</small>" + config.sitio[2]);
+// $("#inicio").html(config.home);
+// $("#nombre").html(perfil.nombre);
+// $("#descripcion").html(perfil.descripcion);
+// $("#column1").html(config.color + "<br/>" + config.libro + "<br/>" + config.musica + "<br/>" + config.video_juego + "<br/>");
+// $("#column2").html(perfil.color + "<br/>" + perfil.libro + "<br/>" + perfil.musica + "<br/>" + perfil.video_juego + "<br/>");
+// $("#lenguajes").html(config.lenguajes);
+// $("#mis_lenguajes").html(perfil.lenguajes[0]);
+// for(i = 1; i < 4; ++i) {
+//     $("#mis_lenguajes").html($("#mis_lenguajes").html() + ", " + perfil.lenguajes[i]);
+// }
+// $("#contacto").html(config.email.replace("[email]", "<a href=\"mailto:danielvieiucv@gmail.com\"> "+ perfil.email + "</a>"));
+// $("#footer").html(config.copyRight);
+// $("#imagen").attr("src", perfil.imagen);
