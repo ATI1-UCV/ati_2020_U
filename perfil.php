8b4878acaf3bbfa42471515dbf4d0eaadf70df83
Aaron Morillo <aaronmorillo7@gmail.com>
2021-05-28 02:45:50 -0400

    archivos reto 20

--- perfil.php
+++ perfil.php
@@ -1,16 +1,18 @@
 <?php 
-			$profile_data = file_get_contents('perfil.json');
-			$decoded_profile_data = json_decode($profile_data, true);
+	session_start();
+	$profile_data = file_get_contents('datos/perfil.json');
+	$decoded_profile_data = json_decode($profile_data, true);
 
-			if($_GET['len'] == 'pt'){
-				$config_data = file_get_contents("conf/configPT.json");				
-			}else if($_GET['len'] == 'en'){
-				$config_data = file_get_contents("conf/configEN.json");				
-			}else if($_GET['len'] == 'es'){
-				$config_data = file_get_contents("conf/configES.json");	
-			}
-			$decoded_config_data = json_decode($config_data, true);
-		
+	if($_GET['len'] == 'pt'){
+		$config_data = file_get_contents("conf/configPT.json");				
+	}else if($_GET['len'] == 'en'){
+		$config_data = file_get_contents("conf/configEN.json");				
+	}else{
+		$config_data = file_get_contents("conf/configES.json");	
+	}
+	$decoded_config_data = json_decode($config_data, true);
+	$_SESSION['nombre'] = $decoded_profile_data['nombre'];
+	$_SESSION['len']= $_GET['len'] ? : "es";
 ?>
 <!DOCTYPE HTML>
 <html>
@@ -19,9 +21,7 @@
 		<meta charset="UTF-8">
 		<link rel="icon" href="http://www.ciens.ucv.ve/portalasig2/favicon.ico" type="image/x-icon">
 		<link rel="stylesheet" href="/css/style.css"  type="text/css">
-		<link rel="stylesheet" href="perfil.css"  type="text/css">
-		<script type="text/javascript" src="config.json"></script>
-		<script type="text/javascript" src="perfil.json"></script>
+		<link rel="stylesheet" href="/css/perfil.css"  type="text/css">
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
@@ -30,7 +30,7 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-		<title>Aaron Morillo</title>
+		<title><?php echo $_SESSION['nombre']; ?></title>
 	</head>
 	<body>
 		
@@ -39,7 +39,7 @@
 				<ul id="header-content" class="col-lg-12">
 					<li id="logo" class="logo col-md-4"><?php echo ($decoded_config_data['sitio'])[0] . '<span>'. ($decoded_config_data['sitio'])[1] .'</span>'. ($decoded_config_data['sitio'])[2]; ?></li>
 
-					<li id="saludo" class="saludo col-md-4"> <?php echo $decoded_config_data['saludo'] . ', ' . $decoded_profile_data['nombre']; ?></li>
+					<li id="saludo" class="saludo col-md-4"> <?php echo $decoded_config_data['saludo'] . ', ' . $_SESSION['nombre']; ?></li>
 
 					<li class="busqueda col-md-3 pull-right"><a id="home" href="../index.html"><?php echo $decoded_config_data['home'] ?></a></li>
 				</ul>	
@@ -48,10 +48,10 @@
 	    <section id="general_content" class="general_content container">
 			<picture>
 				<source media="(max-width:767px)"
-				srcset="27449007first.jpg"
+				srcset="27449007/27449007first.jpg"
 				>
 				<source media="(min-width:768px)"
-				srcset="27449007second.jpg"
+				srcset="27449007/27449007second.jpg"
 				>
 				<img  align="left" id="photo" class="col-lg-5" src="<?php echo $decoded_profile_data['imagen']; ?> ">
 			</picture>
