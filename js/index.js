1edac32cde369d4471c88befb2dbc4c1d6fce8a2
Diego Sánchez <diegosandmg@gmail.com>
2021-05-30 14:34:28 -0400

    adapt student search to server rendered content

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ function renderInner(el, translated) {
   }
 }
 
-function traducir(traducirStr, type) {
+function traducir(traducirStr, type, config) {
   // config and perfil are variables that come from config.json and perfil.json respectively
   traducirElementos = document.getElementsByClassName(traducirStr);
   for (const el of traducirElementos) {
@@ -56,7 +56,8 @@ function renderStudents(listId, students) {
   });
 }
 
-function searchStudent(keyword){
+
+function searchStudent(keyword, listado){
   const result = [];
   for (const student of listado){
     // keyword in student name
@@ -67,34 +68,66 @@ function searchStudent(keyword){
   return result;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+/****
+ * Gets students info from dom
+ * Returns a list of students objects
+ * This function exist so we don't have to fetch
+ * students again.
+ ***/
+function getStudentList(listId){
+  const studentsList = [];
+  const ulElement = document.getElementById(listId);
+  for (const liEl of ulElement.children){
+    studentsList.push(JSON.parse(liEl.dataset.student));
+  }
+  return studentsList;
+}
+document.addEventListener("DOMContentLoaded", async () => {
   // Select elements to translate
-  traducir("traducir-config", "config");
+  const ulStudentsElementId = "students-list";
+  const listado = getStudentList(ulStudentsElementId);
+  const urlParams = new URLSearchParams(window.location.search);
+  const languaje = urlParams.get('len') || "es";
+  const config_dir = "/conf/";
+  const config_prefix = "config";
+  const config_file_type = ".json";
+  let config_file = "";
+  try{
 
-  document.getElementById("especial-logo").innerHTML = `${config.sitio[0]} <small>${config.sitio[1]}</small> ${config.sitio[2]}`;
-
-  // Render student list
-  renderStudents("students-list", listado);
-
-  // Search input
-  const input = document.getElementById("search-students");
-  input.addEventListener("input", ()=>{
-    const students = searchStudent(input.value);
-
-    if (students.length !== 0){
-      renderStudents("students-list", students);
-    }else{
-      // Render an empty student list with a message
-      const ulElement = document.getElementById("students-list");
-      ulElement.innerHTML = "";
-      const template = document.createElement('template');
-      noStudentsMessage = config["noEstudiantes"].replace("[query]", input.value);
-      template.innerHTML = `
-      <li>
-        <p>${noStudentsMessage}</p>
-      </li>
-    `;
-      ulElement.appendChild(template.content.cloneNode(true))
+    if(languaje === "es"){
+      config_file = `${config_dir}${config_prefix}ES${config_file_type}`;
+    }else if(languaje === "en"){
+      config_file = `${config_dir}${config_prefix}EN${config_file_type}`;
+    }else if(languaje === "pt"){
+      config_file = `${config_dir}${config_prefix}PT${config_file_type}`;
     }
-  });
+    const config_response = await fetch(config_file);
+    const config = await config_response.json();
+
+    }catch(error){
+      console.error(error);
+    }
+
+    // Search input
+    const input = document.getElementById("search-students");
+    input.addEventListener("input", ()=>{
+      const students = searchStudent(input.value, listado);
+
+      if (students.length !== 0){
+        renderStudents("students-list", students);
+      }else{
+        // Render an empty student list with a message
+        const ulElement = document.getElementById("students-list");
+        ulElement.innerHTML = "";
+        const template = document.createElement('template');
+
+        noStudentsMessage = `No se encontro al estudiante ${input.value}`;
+        template.innerHTML = `
+        <li>
+          <p>${noStudentsMessage}</p>
+        </li>
+      `;
+        ulElement.appendChild(template.content.cloneNode(true))
+      }
+    });
 });
