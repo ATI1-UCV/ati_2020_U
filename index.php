14e8063ea4f62f210e555d0307c1b3ad4246ced9
Diego Sánchez <diegosandmg@gmail.com>
2021-05-30 14:33:24 -0400

    fix: student rendering list

--- index.php
+++ index.php
@@ -17,6 +17,10 @@
       <?php 
         $estudiantes_json_string = file_get_contents("./datos/index.json");
 
+        $language = $_GET["len"];
+
+        include_once("./get_config.php");
+        $config = get_config($language);
         if(!$estudiantes_json_string){
           echo "<p>No se econtro datos/index.json<p>";
           exit;
@@ -28,6 +32,7 @@
             echo "<p>Unable to decode index/datos.json<p>";
             exit;
         }
+      
       ?>
 	    <header>
 			<nav>
@@ -35,15 +40,15 @@
           <li class="logo" id="especial-logo">
             <?php
               echo($config["sitio"][0]), "<small>", ($config["sitio"][1]), "</small>" ?>
-</li>
+          </li>
           <li class="saludo">
             <span class="traducir-config" data-json-key="saludo"><?php echo($config["saludo"]) ?></span>
             <span class="traducir-perfil" data-json-key="nombre"><?php echo($perfil["nombre"]) ?></span>
           </li>
 					<li class="busqueda">
             <form>
-              <input class="traducir-config" id="search-students" type="search" data-json-key="nombre">
-              <input class="traducir-config" type="button" data-json-key="buscar">
+            <input class="traducir-config" id="search-students" placeholder="<?php echo($config["nombre"]) ?>" type="search" data-json-key="nombre">
+              <input class="traducir-config" type="button" value="<?php echo($config["buscar"]) ?>" data-json-key="buscar">
             </form>
           </li>
 				</ul>	
@@ -52,18 +57,21 @@
 	    <section>
         <ul id="students-list">
         <?php 
-              foreach($students as $student){
-                $li = <<<"EOT"
-                <li>
-                  <img src="$student->{"imagen"})" alt="profile-pic">
-                  <p><a href="index.php?estudiante=$student->{"ci"}"></a>$student->{"nombre"}</p>
+          foreach($estudiantes as $student){
+            $imagen_url = $student["imagen"];
+            $cedula_student = $student["ci"];
+            $student_nombre = $student["nombre"];
+            $json_student = json_encode($student, JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_IGNORE);
+            $li = <<<"EOT"
+            <li data-student='{"ci":"$cedula_student","imagen":"$imagen_url","nombre": "$student_nombre"}'>
+              <img src="$imagen_url" alt="profile-pic">
+              <p><a href="index.php?estudiante=$cedula_student">$student_nombre</a></p>
 
-                </li>
-                EOT;
-                echo $li;
-              }
+            </li>
+            EOT;
+            echo $li;
+          }
         ?>
-
         </ul>
 	    </section>
 	    <footer class="traducir-config" data-json-key="copyRight">
