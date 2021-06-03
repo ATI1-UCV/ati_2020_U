ad2823f121911d9dfeb7e51a4bdea2af445a3c28
Diego Sánchez <diegosandmg@gmail.com>
2021-06-03 18:02:17 -0400

    feat: session for translation

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+  session_start();
+  if(isset($_COOKIE["contador"])){
+    setcookie("contador", $_COOKIE["contador"] + 1);
+  }else{
+    setcookie("contador", 1);
+  }
+
+  $conf_dir = "./conf/conf/";
+
+  $session_lang = isset($_SESSION["language"]);
+  $language = $_GET["len"];
+
+  // if no parameter and language defined
+  if(!$language && $session_lang){
+    $language = $_SESSION["language"];
+  }
+
+  // If no language from parameter, and no session_lang
+  // define default language
+  if(!$language && !$session_lang){
+    $_SESSION["language"] = "es"; 
+    $language = "es";
+  }
+
+  if($language === "es"){
+    $config_json_string = file_get_contents($conf_dir . "configES.json");
+  }elseif ($language === "en"){
+    $config_json_string = file_get_contents($conf_dir . "configEN.json");
+  }elseif ($language === "pt"){
+    $config_json_string = file_get_contents($conf_dir . "configPT.json");
+  }
+
+  if (!$config_json_string){
+    echo "<p>Unable to open language.json<p>";
+  }
+
+  $config = json_decode($config_json_string, true);
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -21,7 +60,16 @@
 				<ul>
 					<li class="logo" id="especial-logo"></li>
           <li class="saludo">
-            <span class="traducir-config" data-json-key="saludo"></span>
+            <span><?php echo($config["saludo"]) ?></span>
+            <span>
+            <?php 
+              if(!isset($_COOKIE["contador"])){
+                echo("(visita " . 1 . " )");
+              }else{
+                echo("(visita " . ($_COOKIE["contador"]) . " )");
+              }
+            ?>
+            </span>
             <span class="traducir-perfil" data-json-key="nombre"></span>
           </li>
 					<li class="busqueda">
@@ -42,8 +90,9 @@
         </div>
 	    </section>
       <section id="student-info-container"></section>
-	    <footer class="traducir-config" data-json-key="copyRight">
-	    </footer>
+      <footer>
+        <?php echo($config["copyRight"]); ?>
+      </footer>
   <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/js/splide.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
