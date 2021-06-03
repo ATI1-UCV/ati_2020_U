f7eaa51103889a617a49d60ec3939c4bf9bc8ec4
@dberbesi <dberbesi@shokworks.io>
2021-06-02 20:05:16 -0400

    archivos reto 19

--- 28015794/perfil.php
+++ 28015794/perfil.php
@@ -1,4 +1,23 @@
-﻿<!DOCTYPE HTML>
+﻿<?php 
+	
+	$perfil = trim(file_get_contents('perfil.json'), "\xEF\xBB\xBF");
+	$Dperfil = json_decode($perfil, true);
+	
+
+	if($_GET['len'] == 'pt'){
+		$config = file_get_contents("conf/configPT.json");				
+	}else if($_GET['len'] == 'en'){
+		$config = file_get_contents("conf/configEN.json");				
+	}else if($_GET['len'] == 'es'){
+		$config = file_get_contents("conf/configES.json");	
+	}else{
+		$config = trim(file_get_contents('config.json'), "\xEF\xBB\xBF");
+	}
+	$Dconfig = json_decode($config, true);
+
+?>
+
+<!DOCTYPE html>
 <html>
 	<head>
 	   	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
@@ -6,8 +25,6 @@
 		<link rel="icon" href="http://www.ciens.ucv.ve/portalasig2/favicon.ico" type="image/x-icon">
 		<link rel="stylesheet" href="../css/style.css"  type="text/css">
 		<link rel="stylesheet" href="perfil.css"  type="text/css">
-		<script type="text/javascript" src="config.json"></script>
-		<script type="text/javascript" src="perfil.json"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
@@ -38,16 +55,16 @@
 	    </section>
 		
 		<script>
-
+		
 			const r11 = document.getElementById("r11");
-			$(r11).text(config.sitio[0]+' '+ config.sitio[1]+' '+ config.sitio[2]);
+			$(r11).text("<?php echo ($Dconfig["sitio"])[0] . ' ' . ($Dconfig["sitio"])[1] . ' ' . ($Dconfig["sitio"])[2]; ?>");
 
 			const r12 = document.getElementById("r12");
-			$(r12).text(config.saludo + perfil.nombre);
+			$(r12).text("<?php echo $Dconfig["saludo"] . ' ' . $Dperfil["nombre"] ?>");
 
 			const r13 = document.getElementById("r13");
 			$(r13).attr("href", "perfil.html");
-			$(r13).text(config.inicio);
+			$(r13).text("<?php echo $Dconfig["inicio"] ?>");
 
 
 			const r21 = document.getElementById("r21");
@@ -57,7 +74,7 @@
 			const myP = document.createElement('p');
 			myP.id = "primero";
 
-			$(myP).text(perfil.nombre);
+			$(myP).text("<?php echo $Dperfil["nombre"] ?>");
 
 			const r22 = document.getElementById("r22");
 			$(r22).append(myP);
@@ -67,7 +84,7 @@
 
 			const myI = document.createElement('i');
 			myI.id = "segundo";
-			$(myI).text(perfil.descripcion);
+			$(myI).text("<?php echo $Dperfil["descripcion"] ?>");
 
 			$(r22).append(myI);
 
@@ -76,7 +93,7 @@
 
 			$(myP2).addClass('tercero');
 
-			$(myP2).text(config.color + perfil.color);
+			$(myP2).text("<?php echo $Dconfig["color"] . ' ' . $Dperfil["color"] ?>");
 			$(r22).append(myP2);
 			$(r22).append(document.createElement('br'));
 
@@ -84,7 +101,7 @@
 			const myP3 = document.createElement('p');
 			$(myP3).addClass('tercero');
 
-			$(myP3).text(config.libro + perfil.libro);
+			$(myP3).text("<?php echo $Dconfig["libro"] . ' ' . $Dperfil["libro"] ?>");
 			$(r22).append(myP3);
 			$(r22).append(document.createElement('br'));
 
@@ -92,7 +109,13 @@
 			const myP4 = document.createElement('p');
 			$(myP4).addClass('tercero');
 
-			$(myP4).text(config.video_juego + perfil.video_juego);
+			$(myP4).text("<?php echo $Dconfig["video_juego"] . ': ';
+			$count = count($Dperfil["video_juego"]);
+			for ($i = 0; $i < $count - 1; $i++) {
+				echo ($Dperfil["video_juego"])[$i] . ', ';
+			}
+			echo ($Dperfil["video_juego"])[$count - 1];
+			?>");
 			$(r22).append(myP4);
 			$(r22).append(document.createElement('br'));
 
@@ -100,7 +123,13 @@
 			const myP5 = document.createElement('p');
 			$(myP5).addClass('tercero', 'extra');
 
-			$(myP5).text(config.lenguajes + perfil.lenguajes);
+			$(myP5).text("<?php echo $Dconfig["lenguajes"] . ': ';
+			$count = count($Dperfil["lenguajes"]);
+			for ($i = 0; $i < $count - 1; $i++) {
+				echo ($Dperfil["lenguajes"])[$i] . ', ';
+			}
+			echo ($Dperfil["lenguajes"])[$count - 1];
+			?>");
 			$(r22).append(myP5);
 			$(r22).append(document.createElement('br'));
 
@@ -109,11 +138,11 @@
 
 			const myP6 = document.createElement('p');
 			$(myP6).addClass('tercero');
-			$(myP6).text(config.email);
+			$(myP6).text("<?php echo $Dconfig["email"] . ' '?>");
 
 			const myA2 = document.createElement('a');
 			$(myA2).attr("href", "https://mail.google.com/mail/?view=cm&fs=1&to=berbesidaniel@gmail.com");
-			$(myA2).text(perfil.email);
+			$(myA2).text("<?php echo $Dperfil["email"] ?>");
 			
 			$(myP6).append(myA2);
 
